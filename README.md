7b9afc2c09f0d9231b2be8b7fa35e52f7147ec99
Iddar Olivares <iddar.olivares.servicios@gmail.com>
2012-06-26 00:16:59 +0000

    Primer deploy solo prueba no es nada del juego.

--- README.md
+++ README.md
@@ -4,10 +4,13 @@ push.js
 Simple juego multijugador en el que el ganador es quien pulse el boton mas rápida mente.
 
 Herramientas utilizadas:
-- Node.js
+- Node.js   
 - HTML5
 - CSS3
 - Javascript
+- Git/GitHub (Control de versiones)
+- Cloud9 (IDE)
+- Heroku (Distribución)
 
-#Notas del Autor:
-Creado con la finalidad de aprender node.js
+##Notas del Autor:
+Creado con la finalidad de aprender node.js 
---
+++ server.js
@@ -0,0 +1,26 @@
+var app = require('http').createServer(handler);
+var io = require('socket.io').listen(app);
+var fs = require('fs');
+
+app.listen(process.env.PORT || 8001);
+
+function handler (req, res) {
+fs.readFile('index.html',
+function (err, data) {
+if (err) {
+res.writeHead(500);
+return res.end('Error loading index.html');
+}
+
+res.writeHead(200, {'Content-Type': 'text/html', "Content-Length": data.length});
+res.end(data);
+});
+}
+
+io.sockets.on('connection', function (socket) {
+// echo the message
+socket.on('message', function (data) {
+console.info(data);
+socket.send("[ECHO] "+data);
+});
+});
